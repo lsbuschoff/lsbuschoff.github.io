17b824374eb9a195f6f4edc08350e046a0c6b0ba
lsbuschoff <62058113+lsbuschoff@users.noreply.github.com>
2026-04-30 18:09:30 -0700

    Added empty title to fix news page

--- src/data/news.ts
+++ src/data/news.ts
@@ -9,26 +9,32 @@ export const newsData: News[] = [
   // If you don't want to show news, just make the array empty.
   {
     date: "May 2026",
+    title: "",
     description: "Our paper 'Can vision language models learn intuitive physics from interaction' was accepted to ICML'26.",
   },
   {
     date: "April 2026",
+    title: "",
     description: "I was named one of the top 200 reviewers for ICLR'26 (~1%).",
   },
   {
     date: "April 2026",
+    title: "",
     description: "I gave a talk in the CogToolsLab at Stanford.",
   },
   {
     date: "April 2026",
+    title: "",
     description: "I gave a talk in the Enigma project at Stanford.",
   },
   {
     date: "March 2026",
+    title: "",
     description: "I gave a talk in the Causality in Cognition lab at Stanford.",
   },
   {
     date: "February 2026",
+    title: "",
     description: "I'm visiting Tobias Gerstenberg's lab in Stanford until May.",
   }
 ];
