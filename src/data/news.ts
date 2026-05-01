38846c18a624e71321fcf9ec693d8a229bb52bf3
lsbuschoff <62058113+lsbuschoff@users.noreply.github.com>
2026-04-30 18:07:34 -0700

    Updated news section with news from March and April

--- src/data/news.ts
+++ src/data/news.ts
@@ -8,8 +8,27 @@ export interface News {
 export const newsData: News[] = [
   // If you don't want to show news, just make the array empty.
   {
+    date: "May 2026",
+    description: "Our paper 'Can vision language models learn intuitive physics from interaction' was accepted to ICML'26.",
+  },
+  {
+    date: "April 2026",
+    description: "I was named one of the top 200 reviewers for ICLR'26 (~1%).",
+  },
+  {
+    date: "April 2026",
+    description: "I gave a talk in the CogToolsLab at Stanford.",
+  },
+  {
+    date: "April 2026",
+    description: "I gave a talk in the Enigma project at Stanford.",
+  },
+  {
+    date: "March 2026",
+    description: "I gave a talk in the Causality in Cognition lab at Stanford.",
+  },
+  {
     date: "February 2026",
-    title: "Research visit at Stanford",
-    description: "I'm visiting Tobias Gerstenberg's lab in Stanford until May. Let me know if you are in the Bay and want to meet!",
+    description: "I'm visiting Tobias Gerstenberg's lab in Stanford until May.",
   }
 ];
